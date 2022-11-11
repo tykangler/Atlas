1f0fe0cbffcdd45775b6ec6dc3fa920fbff02251
Tyler Kang <tylerkang@hotmail.com>
2022-11-11 11:44:05 -0800

    update solution

---
+++ src/Atlas.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Atlas.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Atlas.Web.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Atlas.Web.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
