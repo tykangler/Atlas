b04b9ce343bf2899f35347a2aec94618915b429f
Tyler Kang <tylerkang@hotmail.com>
2022-12-27 01:34:10 -0800

    rename entityrelationshipextractor to coreferenceresolver because name makes more sense

---
+++ src/Atlas.CoreferenceResolver/server.py
@@ -0,0 +1,16 @@
+# inputs:
+#   plaintext article
+
+# procedure:
+#   ...
+
+# outputs:
+#   list of 
+
+# can pass in plaintext article,
+# returning: 
+# + list of antecedent indices with subjects
+
+# can pass in list of tokens,
+# returning:
+# + same list of tokens with antecedents marked
